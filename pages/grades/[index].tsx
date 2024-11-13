bcbe1a7ae662da7de624376c28c06a929b204771
Jonathan Shapiro <50190184+Jshap06@users.noreply.github.com>
2024-11-13 10:03:52 -0500

    Update [index].tsx

--- pages/grades/[index].tsx
+++ pages/grades/[index].tsx
@@ -240,7 +240,7 @@ export default function Grades({
 												updateOptimize(e.target.value, "desiredGrade")
 											}
 											className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-											placeholder={course.gradeScale ? String(course.gradeScale[Object.keys(course.gradeScale)[0]][0]): "90"}
+											placeholder={course.gradingScale ? String(course.gradingScale[Object.keys(course.gradingScale)[0]][0]): "90"}
 										/>
 									</div>
 								</div>
